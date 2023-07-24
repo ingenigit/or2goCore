77313350cf7a62c34b3ad9e442d89b44fb83821f
Jagannath <jagannath@ingeni-systems.com>
2023-07-24 13:21:59 +0530

    Delivery address structire added locality, sublocality fields.

--- core/src/main/java/com/or2go/core/DeliveryAddrInfo.java
+++ core/src/main/java/com/or2go/core/DeliveryAddrInfo.java
@@ -4,28 +4,34 @@ public class DeliveryAddrInfo {
     public String nickname;
     public String addr;
     public String place;
+    public String locality;
+    public String sublocality;
     public String landmark;
     public String zipcode;
     public String latest;
     public String altcontact;
     public String geoposition;
 
-    public DeliveryAddrInfo(String name, String adr, String loc, String mark, String pin, String altaddr)
+    public DeliveryAddrInfo(String name, String adr, String plc, String loc, String subloc, String mark, String pin, String altaddr)
     {
         nickname= name;
         addr=adr;
-        place=loc;
+        place=plc;
+        locality = loc;
+        sublocality = subloc;
         landmark=mark;
         zipcode=pin;
         altcontact=altaddr;
         latest="";
     }
 
-    public void updateAddressInfo(String name, String adr, String loc, String mark, String pin, String altaddr)
+    public void updateAddressInfo(String name, String adr, String plc, String loc, String subloc, String mark, String pin, String altaddr)
     {
         nickname= name;
         addr=adr;
-        place=loc;
+        place=plc;
+        locality = loc;
+        sublocality = subloc;
         landmark=mark;
         zipcode=pin;
         altcontact=altaddr;
@@ -35,12 +41,14 @@ public class DeliveryAddrInfo {
     public String getAddress() { return addr;}
     public String getLandmark() { return landmark;}
     public String getPlace() { return place;}
+    public String getLocality() { return locality;}
+    public String getSublocality() { return sublocality;}
     public String getZipCode(){return zipcode;}
     public String getAltcontact() {return altcontact;}
 
     public String getCompleteAddress()
     {
-        return (addr+"Place-"+place+" Landmark-"+landmark+" PIN-"+zipcode+" Contact-"+altcontact);
+        return (addr+"Place-"+place+" Locality-"+locality+" Landmark-"+landmark+" PIN-"+zipcode+" Contact-"+altcontact);
     }
 
     public void setGeoPosition(String geocode) { geoposition = geocode;}
