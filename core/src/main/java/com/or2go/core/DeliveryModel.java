38fc7fbc97bf11991dc6cbc6158ed9a1f9d725bf
Jagannath <jagannath@ingeni-systems.com>
2023-07-25 21:35:40 +0530

    Delivery Model updated with State, Place fields.

--- core/src/main/java/com/or2go/core/DeliveryModel.java
+++ core/src/main/java/com/or2go/core/DeliveryModel.java
@@ -4,13 +4,17 @@ public class DeliveryModel {
     public Integer mId;
     public Integer mType;
     public String  mName;
+    public String  mState;
+    public String  mPlace;
     public String  mParams;
 
-    public DeliveryModel(Integer id, Integer type, String name, String param)
+    public DeliveryModel(Integer id, Integer type, String name, String state, String place, String param)
     {
         mId = id;
         mType = type;
         mName = name;
+        mState = state;
+        mPlace = place;
         mParams = param;
     }
 }
