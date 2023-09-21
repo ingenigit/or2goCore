df13619405bccbcdb40525fce96b82003474f50d
Jagannath <jagannath@ingeni-systems.com>
2023-09-21 15:38:17 +0530

    Added member function to OrderDeliveryInfo.

--- core/src/main/java/com/or2go/core/OrderDeliveryInfo.java
+++ core/src/main/java/com/or2go/core/OrderDeliveryInfo.java
@@ -1,10 +1,26 @@
 package com.or2go.core;
 
 public class OrderDeliveryInfo {
-    String orderid;
-    String daid;
-    String daname;
-    String dacontact;
-    String delitime;
-    Integer deliverystatus;
+    public String orderid;
+    public String daid;
+    public String daname;
+    public String dacontact;
+    public String delitime;
+    public Integer delistatus;
+
+    public OrderDeliveryInfo(String oid, String did, String name, String contact, String time, Integer sts)
+    {
+        orderid=oid;
+        daid= did;
+        daname = name;
+        dacontact=contact;
+        delitime =time;
+        delistatus=sts;
+    }
+
+    public String getDAName() {return daname;}
+    public String getDAContact() {return dacontact;}
+
+    public String getDeliveryTime() {return delitime;}
+    public Integer getDeliveryStatus() {return delistatus;}
 }
