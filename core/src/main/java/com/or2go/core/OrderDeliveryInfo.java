4f0677c90b9305ad9fd309548e0ae8033a6bbadc
Jagannath <jagannath@ingeni-systems.com>
2023-09-21 14:40:55 +0530

    Added OrderDeliveryInfo class to store order delivery DA information.

---
+++ core/src/main/java/com/or2go/core/OrderDeliveryInfo.java
@@ -0,0 +1,10 @@
+package com.or2go.core;
+
+public class OrderDeliveryInfo {
+    String orderid;
+    String daid;
+    String daname;
+    String dacontact;
+    String delitime;
+    Integer deliverystatus;
+}
